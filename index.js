63b5ea51e83c51c9f1062407174c1c8d96997e84
Gaurav <munjewargaurav@gmail.com>
2022-01-11 15:09:12 +0530

    setup db

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express= require('express');
+const expressLayouts=require('express-ejs-layouts');
 
 const port=8000; // 80 as in default production server
 const app= express();
+app.use(expressLayouts);
+
+app.use(express.static('./assets'));
+
+//extracting script and styles
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
 
 app.use('/',require('./routes'));
 
